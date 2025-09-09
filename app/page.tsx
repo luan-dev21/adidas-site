654f913390ddd79b88d1725851b18d14b983476c
AliR <ali.sanatii1397@gmail.com>
2025-09-09 14:26:16 +0330

    Modify folder structure and Add ViewCanvas & MainStudioModel

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Test from "@/components/Test";
+import ViewCanvas from "@/components/ViewCanvas";
 import Image from "next/image";
 
 export default function Home() {
-  return <Test />;
+  return <ViewCanvas />;
 }
