bb514a8a16e22005fbc18364d583651ec556fe29
AliR <ali.sanatii1397@gmail.com>
2025-09-24 15:30:55 +0800

    Add html layers like Header.tsx, Footer.tsx, MusicStreamLine.tsx & scrollIndicator.tsx for adding

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Mulish } from "next/font/google";
 import "./globals.css";
 import ViewCanvas from "@/components/ViewCanvas";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const mulish = Mulish({
   subsets: ["cyrillic"],
@@ -22,7 +24,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${mulish.className} antialiased`}>
         <ViewCanvas />
+        <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
